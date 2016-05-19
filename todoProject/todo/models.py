ae5c05b58fea19c55b6e6ead04a5e50dbb8bd8e5
gragunathan <gragunathan@ebay.com>
2016-05-18 22:13:32 -0400

    created todo tables

--- todoProject/todo/models.py
+++ todoProject/todo/models.py
@@ -3,3 +3,8 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Todo(models.Model):
+	task_name = models.CharField(max_length=200)
+	task_status = models.BooleanField(default=False)
+
